dd9cd437a278a285da5df86cd94d5c4055714462
vinki100z <vikrantchauhancf2021@gmail.com>
2024-10-05 00:40:08 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-app.use(cors());
+app.use(cors({
+  origin: 'https://unity-score2-0-m94p9mk05-vikrant-chauhans-projects-59869ee4.vercel.app', // Your frontend URL
+  methods: 'GET,POST', // Specify allowed methods
+  credentials: true, // If you need to allow cookies or authentication headers
+}));
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
