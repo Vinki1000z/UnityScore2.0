111ef8962fd703d4d1ac428a299a236f8adb8ff7
vinki1000z <112200932+Vinki1000z@users.noreply.github.com>
2024-10-05 01:37:44 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,32 +10,6 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
-
-app.use(cors({
-  origin: 'https://unity-score2-0-m94p9mk05-vikrant-chauhans-projects-59869ee4.vercel.app', // Replace this with your actual frontend URL
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
-
-// Alternatively, using custom headers:
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://unity-score2-0-m94p9mk05-vikrant-chauhans-projects-59869ee4.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
