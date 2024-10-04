c7e300b0ac6d9a13fac4a3f319021f5328dd9eea
vinki100z <vikrantchauhancf2021@gmail.com>
2024-10-05 01:14:18 +0530

    frontend

--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
-const URL ="https://unity-score2-0-backend-i6czcqo16.vercel.app/";
+const URL ="https://unity-score2-0-backend-i6czcqo16.vercel.app";
 
 function Login() {
   const [email, setEmail] = useState('');
